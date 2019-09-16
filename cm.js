e595fad30abbc7bf8f386347a6770524d82f23f1
madyabaaaaaaa <53091979+madyabaaaaaaa@users.noreply.github.com>
2019-09-16 23:42:49 +0700

    Update cm.js

--- cm.js
+++ cm.js
@@ -1,6 +1,6 @@
 var inputField = document.querySelector('.input');
 var outputField = document.querySelector('.output');
-const PLUSSIZE = 3;
+const PLUSSIZE = 2.5;
 function getValue() {
   if (inputField.value != '') {
     outputField.innerHTML = parseInt(inputField.value)  + PLUSSIZE;
